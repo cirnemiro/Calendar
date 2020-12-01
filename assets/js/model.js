bfda6764697ae52902712384785de7d80badb51a
cirnemiro <cirnemiro@gmail.com>
2020-12-01 17:59:33 +0100

    local storage years

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,6 +1,51 @@
 let getKeys20 = localStorage.getItem('2020')
 let getKeys21 = localStorage.getItem('2021')
 
+for( let i = 1; i<=12; i++){
+    year20[i]=[]
+    if(i==3||i==1||i==5||i==7||i==8||i==10||i==12){
+        for( let j = 1; j<=31; j++){
+            year20[i][j]= ""
+            
+        }
+    }else if(i==2){
+        for( let j = 1; j<=29; j++){
+            year20[i][j]= ""
+            
+        }
+    }
+    else{
+        for( let j = 1; j<=30; j++){
+            year20[i][j]= ""
+            
+        }
+    }
+}
+for( let i = 1; i<=12; i++){
+    year21[i]=[]
+    if(i==3||i==1||i==5||i==7||i==8||i==10||i==12){
+        for( let j = 1; j<=31; j++){
+            year21[i][j]= ""
+            
+        }
+    }else if(i==2){
+        for( let j = 1; j<=28; j++){
+            year21[i][j]= ""
+            
+        }
+    }
+    else{
+        for( let j = 1; j<=30; j++){
+            year21[i][j]= ""
+            
+        }
+    }
+}
+
+
+
+
+
 
 if(getKeys20 === null){
     localStorage.setItem('2020',JSON.stringify(year20))
@@ -19,50 +64,6 @@ if(getKeys21 === null){
     console.log('key estaba creado');
 }
 
-
-function start(){
-    for( let i = 1; i<=12; i++){
-        year20[i]=[]
-        if(i==3||i==1||i==5||i==7||i==8||i==10||i==12){
-            for( let j = 1; j<=31; j++){
-                year20[i][j]= ""
-        
-            }
-        }else if(i==2){
-            for( let j = 1; j<=29; j++){
-                year20[i][j]= ""
-        
-            }
-        }
-        else{
-            for( let j = 1; j<=30; j++){
-                year20[i][j]= ""
-        
-            }
-        }
-    }
-    for( let i = 1; i<=12; i++){
-        year21[i]=[]
-        if(i==3||i==1||i==5||i==7||i==8||i==10||i==12){
-            for( let j = 1; j<=31; j++){
-                year21[i][j]= ""
-        
-            }
-        }else if(i==2){
-            for( let j = 1; j<=28; j++){
-                year21[i][j]= ""
-        
-            }
-        }
-        else{
-            for( let j = 1; j<=30; j++){
-                year21[i][j]= ""
-        
-            }
-        }
-    }
-}
-start()
 
 
 
